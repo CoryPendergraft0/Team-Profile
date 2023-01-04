a360442f5e99316ee0b8b78ff624ebe5f5454b2d
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-01-03 19:38:08 -0500

    added first prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,29 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const generateHTML = require('./src/page-template');
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const teamArray = [];
+
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Who is the Manager?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the Managers name');
+                    return false;
+                }
+            }
+        },
+        
+    ])
+}

31d3d7eeb7378b231d4284bbf03de6fcd9f42248
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-01-03 21:05:05 -0500

    added more prompts

--- Develop/index.js
+++ Develop/index.js
@@ -9,34 +9,53 @@ const inquirer = require('inquirer');
 
 const teamArray = [];
 
-const addManager = () => {
-    return inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Who is the Manager?',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the Managers name');
-                    return false;
-                }
+const questions = [
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What role is the employee?',
+        choices: ['Manager', 'Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the employee?',
+        validate: nameInput => {
+            if (nameInput)
+            {
+            return true;
+            } else {
+            console.log('You must enter a name');
+            return false;
+            }}
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the employee ID?',
+        validate: idInput => {
+            if (idInput)
+            {
+            return true;
+            } else {
+                console.log('Please enter an ID number');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Please enter the managers ID',
-            validate: nameInput => {
-                if (isNaN(nameInput)) {
-                    console.log('Please enther the ID')
-                    return false;
-                } else {
-                    return true;
-                }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is this employees email?',
+        validate: emailInput => {
+            if (emailInput)
+            {
+            return true;
+            } else {
+                console.log('Please enter the employees email');
+                return false;
             }
-        },
-    ])
-}
-addManager();
+        }
+    },
+
+]

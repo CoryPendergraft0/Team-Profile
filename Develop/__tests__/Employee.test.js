68ea241fb3153ae74dc87401bbbbdf71476c6407
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-01-03 22:30:41 -0500

    finshed employee and engineer tests to PASS

--- Develop/__tests__/Employee.test.js
+++ Develop/__tests__/Employee.test.js
@@ -1,5 +1,9 @@
 const Employee = require("../lib/Employee");
 
-describe("Employee", () => {
+test('creates employee section', () => {
+    const employee = new Employee('Cory', 1234, 'Cory1234@gmail.com');
 
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.email).toContain('@');
+    expect(employee.id).toEqual(expect.any(Number));
 });
--- Develop/__tests__/Engineer.test.js
+++ Develop/__tests__/Engineer.test.js
@@ -1,5 +1,6 @@
 const Engineer = require("../lib/Engineer");
 
-describe("Engineer", () => {
-
-});
+test('creates engineer section', () => {
+    const engineer = new Engineer('Cory', 1234, 'cory123@gmail', 'CoryP123');
+    expect(engineer.github).toEqual(expect.any(String));
+})

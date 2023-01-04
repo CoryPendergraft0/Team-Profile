748c1204f82c65f22f72b086ac777671f0a79d96
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-01-03 21:42:41 -0500

    holy cow it works!

--- Develop/index.js
+++ Develop/index.js
@@ -21,22 +21,21 @@ const questions = [
         name: 'name',
         message: 'What is the name of the employee?',
         validate: nameInput => {
-            if (nameInput)
-            {
-            return true;
+            if (nameInput) {
+                return true;
             } else {
-            console.log('You must enter a name');
-            return false;
-            }}
+                console.log('You must enter a name');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'id',
         message: 'What is the employee ID?',
         validate: idInput => {
-            if (idInput)
-            {
-            return true;
+            if (idInput) {
+                return true;
             } else {
                 console.log('Please enter an ID number');
                 return false;
@@ -48,9 +47,8 @@ const questions = [
         name: 'email',
         message: 'What is this employees email?',
         validate: emailInput => {
-            if (emailInput)
-            {
-            return true;
+            if (emailInput) {
+                return true;
             } else {
                 console.log('Please enter the employees email');
                 return false;
@@ -61,40 +59,80 @@ const questions = [
         type: 'input',
         name: 'officeNumber',
         message: 'What is this employees office number?',
-        when:(officeNumberInput) => officeNumberInput.role === 'Manager',
+        when: (officeNumberInput) => officeNumberInput.role === 'Manager',
         validate: officeNumberInput => {
             if (officeNumberInput) {
                 return true;
             } else {
                 console.log('Please enter the office number');
                 return false;
-            }}
+            }
+        }
     },
     {
         type: 'input',
         name: 'github',
         message: 'What is this employees github username?',
-        when:(githubInput) => githubInput.role === 'Engineer',
+        when: (githubInput) => githubInput.role === 'Engineer',
         validate: githubInput => {
             if (githubInput) {
                 return true;
             } else {
                 console.log('Please enter the Engineers github username');
                 return false;
-            }}
+            }
+        }
     },
     {
         type: 'input',
         name: 'school',
         message: 'What is this interns school?',
-        when:(schoolInput) => schoolInput.role === 'Intern',
+        when: (schoolInput) => schoolInput.role === 'Intern',
         validate: schoolInput => {
             if (schoolInput) {
                 return true;
             } else {
                 console.log('Please enter the interns University');
                 return false;
-            }}
+            }
+        }
     },
-    
-]
+    {
+        type: 'confirm',
+        name: 'confirmAddEmployee',
+        message: 'Would you like to add another employee to you team?',
+        default: false
+    }
+];
+
+const promptEmployee = () => {
+    console.log(`
+    ==================
+    Add a New Employee
+    ==================
+    `);
+
+    return inquirer.prompt(questions)
+    .then(employeeData => {
+        let {role, name, id, email, officeNumber, github, school} = employeeData;
+        let employee;
+    if (role === 'Manager'){
+        employee = new Manager(name, id, email, officeNumber);
+    }
+    if (role === 'Engineer'){
+        employee = new Engineer(name, id, email, github);
+    }
+    if (role === 'Intern'){
+        employee = new Intern(name, id, email, school);
+    }
+    teamArray.push(employee);
+
+    if (employeeData.confirmAddEmployee) {
+        return promptEmployee(teamArray);
+    } else {
+        return teamArray;
+    }
+}
+)
+};
+promptEmployee();
